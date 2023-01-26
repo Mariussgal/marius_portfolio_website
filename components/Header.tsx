550f8027ab905da104fc39e7890f6e73a0206a4e
JEAN GAL <j.c.r.gal@student.tudelft.nl>
2023-01-26 13:28:27 +0100

    Basis for home page. Added all elements + fixing viewport for header and body

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,33 +1,40 @@
 import { ConnectButton } from '@rainbow-me/rainbowkit';
-import { Flex, Text, HStack, LinkBox, LinkOverlay, Spacer } from "@chakra-ui/react";
+import { Flex, Text, HStack, LinkBox, LinkOverlay, Spacer, Box } from "@chakra-ui/react";
 import NextLink from "next/link"
 import Medias from './Medias';
 
 export default function Header() {
 
-    const siteTitle = "Jean Gal"
+    const siteTitle = "Home"
 
     return (
-        <Flex>
-            <HStack spacing='50px'>
+        <Flex h="10vh" ml="2%" mr="2%" borderBottomColor="black">
+            <HStack spacing="4vw" w="15%">
                 <LinkBox>
                     <NextLink href={'/'} passHref>
                         <LinkOverlay>
-                            <Text fontSize="xl" fontWeight="bold">{siteTitle}</Text>
+                            <Text fontSize="lg" fontWeight="bold">{siteTitle}</Text>
                         </LinkOverlay>
                     </NextLink>
                 </LinkBox>
                 <LinkBox>
                     <NextLink href={'/work'} passHref>
                         <LinkOverlay>
-                            <Text fontSize="xl" fontWeight="bold">{"Work"}</Text>
+                            <Text fontSize="lg" fontWeight="bold">{"Work"}</Text>
                         </LinkOverlay>
                     </NextLink>
                 </LinkBox>
                 <LinkBox>
                     <NextLink href={'/about'} passHref>
                         <LinkOverlay>
-                            <Text fontSize="xl" fontWeight="bold">{"About"}</Text>
+                            <Text fontSize="lg" fontWeight="bold">{"About"}</Text>
+                        </LinkOverlay>
+                    </NextLink>
+                </LinkBox>
+                <LinkBox>
+                    <NextLink href={'/nfts'} passHref>
+                        <LinkOverlay>
+                            <Text fontSize="lg" fontWeight="bold">{"NFTs"}</Text>
                         </LinkOverlay>
                     </NextLink>
                 </LinkBox>
@@ -35,7 +42,9 @@ export default function Header() {
             <Spacer/>
             <Medias/>
             <Spacer/>
-            <ConnectButton />
+            <Box w="15%">
+                <ConnectButton />
+            </Box>
         </Flex>
     );
 }
