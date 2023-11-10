2d363d23772f15eec5aa36a2509daf8e822ba409
JEAN GAL <jgatbkk@gmail.com>
2023-11-10 11:52:37 -0500

    Add poaps to next page for showcasing

--- pages/nfts.tsx
+++ pages/nfts.tsx
@@ -1,12 +1,36 @@
 import type { NextPage } from 'next';
-import { Container } from "@chakra-ui/layout";
+import { Flex, Box, Text, Image } from "@chakra-ui/react";
+import { useState, useEffect } from 'react';
+import { fetchPoapData } from '../requests/fetchPoaps';
+import { POAP } from '../ts/types';
+
 
 const nfts: NextPage = () => {
+  
+  const [poaps, setPoaps] = useState<POAP[]>([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const data = await fetchPoapData();
+      setPoaps(data);
+    };
+    fetchData();
+  }, []);
+  
+  useEffect(() => {
+    console.log('Poaps state:', poaps);
+  }, [poaps]);
+
   return (
     <>
-      <Container>
-    
-      </Container>
+    <Flex flexDirection="column">
+      {poaps.length > 0 ? poaps.map((poap) => (
+        <Box key={poap.tokenId}>
+          <Text>{poap.event.name}</Text>
+          <Image src={poap.event.image_url} alt={poap.event.name} />
+        </Box>
+      )) : <Text>Loading...</Text>}
+    </Flex>
     </>
   )
 }
