061b22376092c3a84d058ca4d5f1494116fd6953
JEAN GAL <jgatbkk@gmail.com>
2023-11-16 01:08:03 -0500

    creating cards for projects

--- pages/projects.tsx
+++ pages/projects.tsx
@@ -1,13 +1,21 @@
 import type { NextPage } from 'next';
-import { Container } from "@chakra-ui/layout";
+import { Box, Text, Flex, Center, VStack, Image } from '@chakra-ui/react';
 
 const projects: NextPage = () => {
+
+  const cards = [1, 2, 3, 4, 5];
+
   return (
-    <>
-      <Container>
-    
-      </Container>
-    </>
+    <Flex flexGrow={1} alignItems="center" justifyContent="center">
+        <Flex overflowX="scroll">
+          {cards.map((card) => (
+            <Box minWidth="300px" height="400px" p="5" bg="white" boxShadow="md" rounded="md" key={card} mr={4}>
+              <Image src="https://via.placeholder.com/150" alt="Image" />
+              <Text mt="5">Card {card}</Text>
+            </Box>
+          ))}
+        </Flex>
+    </Flex>
   )
 }
 
