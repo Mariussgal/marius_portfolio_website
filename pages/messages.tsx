c968da25c28065042e311fb89728f791ed2d8851
JEAN GAL <jgatbkk@gmail.com>
2024-01-21 20:55:04 -0500

    deleted xmtp messaging component

--- pages/messages.tsx
+++ pages/messages.tsx
@@ -1,12 +1,11 @@
 import type { NextPage } from 'next';
 import { Container } from "@chakra-ui/layout";
-import { StartConversation } from '../components/Messaging/StartConv';
 
 const messages: NextPage = () => {
   return (
     <>
       <Container>
-        <StartConversation/>
+
       </Container>
     </>
   )
