12a5eb2583ba894d69316f4247e1ef74214e06cc
JEAN GAL <jgatbkk@gmail.com>
2024-01-23 15:36:53 -0500

    simplified medias by making IconButtons a tags

--- components/Medias.tsx
+++ components/Medias.tsx
@@ -1,33 +1,29 @@
-import { Box, Flex, IconButton } from '@chakra-ui/react'
+import { Box, Flex, IconButton } from '@chakra-ui/react';
 import { FaLinkedin, FaGithub, FaEnvelope, FaTwitter } from "react-icons/fa";
 
 export default function Medias() {
     return (
         <Box position="fixed" right="0" top="50%">
             <Flex direction="column" align="center" justify="center" transform="translateY(-50%)">
-                <IconButton
+                <IconButton as="a" href="mailto:jeangal.pro@gmail.com" target="_blank"
                     aria-label="Mail"
                     icon={<FaEnvelope size="24px" />}
                     color="black"
-                    onClick={() => window.open('mailto:jeangal.pro@gmail.com', '_blank')}
                 />
-                <IconButton
+                <IconButton as="a" href="https://twitter.com/jeangalbc" target="_blank"
                     aria-label="Twitter"
                     icon={<FaTwitter size="24px" />}
                     color="black"
-                    onClick={() => window.open('https://twitter.com/jeangalbc', '_blank')}
                 />
-                <IconButton
+                <IconButton as="a" href="https://www.linkedin.com/in/jean-gal/" target="_blank"
                     aria-label="LinkedIn"
                     icon={<FaLinkedin size="24px" />}
                     color="black"
-                    onClick={() => window.open('https://www.linkedin.com/in/jean-gal/', '_blank')}
                 />
-                <IconButton
+                <IconButton as="a" href="https://github.com/jjeangal" target="_blank"
                     aria-label="Github"
                     icon={<FaGithub size="24px" />}
                     color="black"
-                    onClick={() => window.open('https://github.com/jjeangal', '_blank')}
                 />
             </Flex>
         </Box>
