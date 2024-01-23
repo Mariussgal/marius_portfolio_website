07e89f41fbd5410c3db709f8ed44e9512ef272e6
JEAN GAL <jgatbkk@gmail.com>
2024-01-23 15:41:58 -0500

    simplified header component by: - removing web3 connect button - changing links to pages (MAIN ERROR - hydratation - a nested in a tag)

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,56 +1,31 @@
-import { ConnectButton } from '@rainbow-me/rainbowkit';
-import { Flex, Text, HStack, LinkBox, LinkOverlay, Spacer, Box } from "@chakra-ui/react";
+import { Flex, Text, HStack, Spacer, Box, Link } from "@chakra-ui/react";
 import NextLink from "next/link"
 import Medias from './Medias';
 
 export default function Header() {
-
     const siteTitle = "Home"
 
     return (
         <Flex h="10vh" borderBottom="1px">
             <HStack spacing="4vw" w="30%" ml="2%">
-                <LinkBox>
-                    <NextLink href={'/'} passHref>
-                        <LinkOverlay>
-                            <Text fontSize="lg" fontWeight="bold">{siteTitle}</Text>
-                        </LinkOverlay>
-                    </NextLink>
-                </LinkBox>
-                <LinkBox>
-                    <NextLink href={'/projects'} passHref>
-                        <LinkOverlay>
-                            <Text fontSize="lg" fontWeight="bold">{"Work"}</Text>
-                        </LinkOverlay>
-                    </NextLink>
-                </LinkBox>
-                <LinkBox>
-                    <NextLink href={'/about'} passHref>
-                        <LinkOverlay>
-                            <Text fontSize="lg" fontWeight="bold">{"About"}</Text>
-                        </LinkOverlay>
-                    </NextLink>
-                </LinkBox>
-                <LinkBox>
-                    <NextLink href={'/poaps'} passHref>
-                        <LinkOverlay>
-                            <Text fontSize="lg" fontWeight="bold">{"POAPs"}</Text>
-                        </LinkOverlay>
-                    </NextLink>
-                </LinkBox>
-                <LinkBox>
-                    <NextLink href={'/messages'} passHref>
-                        <LinkOverlay>
-                            <Text fontSize="lg" fontWeight="bold">{"Messages"}</Text>
-                        </LinkOverlay>
-                    </NextLink>
-                </LinkBox>
+                <Box>
+                    <Link href="/" fontSize="lg" fontWeight="bold">{siteTitle}</Link>
+                </Box>
+                <Box>
+                    <Link href="/projects" fontSize="lg" fontWeight="bold">Work</Link>
+                </Box>
+                <Box>
+                    <Link href="/about" fontSize="lg" fontWeight="bold">About</Link>
+                </Box>
+                <Box>
+                    <Link href="/poaps" fontSize="lg" fontWeight="bold">POAPs</Link>
+                </Box>
             </HStack>
             <Spacer />
             <Medias />
             <Spacer />
             <Flex w="30%" alignItems="center" justifyContent="right" mr="2%">
-                <ConnectButton />
+                <Box>web3 button</Box >
             </Flex>
         </Flex>
     );
