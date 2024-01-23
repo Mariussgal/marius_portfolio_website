7b337d4bbe7d1433b6e30ea175f80f09048d3dd8
JEAN GAL <jgatbkk@gmail.com>
2024-01-23 15:37:11 -0500

    removed unused web3 providers and wallet connection

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,49 +1,13 @@
-import '@rainbow-me/rainbowkit/styles.css';
 import type { AppProps } from 'next/app';
 import { ChakraProvider } from '@chakra-ui/react'
-import { RainbowKitProvider, getDefaultWallets } from '@rainbow-me/rainbowkit';
-import { configureChains, createClient, WagmiConfig } from 'wagmi';
-import { mainnet, polygon, goerli } from 'wagmi/chains';
-import { alchemyProvider } from 'wagmi/providers/alchemy';
-import { publicProvider } from 'wagmi/providers/public';
 import Layout from './layout';
-
-const { chains, provider, webSocketProvider } = configureChains(
-  [
-    mainnet,
-    polygon,
-    ...(process.env.NEXT_PUBLIC_ENABLE_TESTNETS === 'true' ? [goerli] : []),
-  ],
-  [
-    alchemyProvider({
-      apiKey: '0N1EEy-xUfCUvsD0c_j7hiaoebNl76uX',
-    }),
-    publicProvider(),
-  ]
-);
-
-const { connectors } = getDefaultWallets({
-  appName: 'RainbowKit App',
-  chains,
-});
-
-const wagmiClient = createClient({
-  autoConnect: true,
-  connectors,
-  provider,
-  webSocketProvider,
-});
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider>
-      <WagmiConfig client={wagmiClient}>
-        <RainbowKitProvider chains={chains}>
-          <Layout>
-            <Component {...pageProps} />
-          </Layout>
-        </RainbowKitProvider>
-      </WagmiConfig>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
     </ChakraProvider>
   );
 }
