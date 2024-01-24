eca53bc9e804487894865e8681f9110c54c9abde
JEAN GAL <jgatbkk@gmail.com>
2024-01-24 11:43:14 -0500

    - Changed structure of home page - Added animation at the bottom of the screen

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,38 +1,40 @@
-import { Box, Center, Divider, Flex, Image } from '@chakra-ui/react';
+import { Box, Divider, Flex, Image } from '@chakra-ui/react';
+import { motion } from 'framer-motion';
 import type { NextPage } from 'next';
 
 const Home: NextPage = () => {
   return (
-    <Center as="div" height="90vh">
-      <Flex flexDirection="row" height="100%" color="black">
-        <Box 
-          position="absolute" 
-          height="200px"
-          width="200px" 
-          top="50%" 
-          left="50%" 
-          mt="-100px" 
-          ml="-100px" 
-          zIndex="1"
-          _hover={{
-            height: "400px",
-            width: "400px",
-            mt: "-200px",
-            ml: "-200px"
-          }}
-        >
-          <Image src={"./mepic.png"} alt='Jean Gal' />
-        </Box>
+    <Flex flexDirection="column" height="90vh" width="100%" color="black">
+      <Box
+        height="60%"
+        width="100%"
+        display="flex"
+        alignItems="center"
+        justifyContent="center"
+      >
+        <Image src={"./mepic.png"} alt='Jean Gal' height="60%" width="100%" objectFit="contain" />
+      </Box>
+      <Flex flexDirection="row" alignItems="center" justifyContent="center" w="100%" height="30%">
         <Flex alignItems="center" justifyContent="center" w="50%">
-          <p>Hi, my name is Jean Gal <br/> and welcome to my website</p>
+          <p>Hi, my name is Jean Gal <br /> and welcome to my website</p>
         </Flex>
-        <Divider orientation='vertical' size="5px" width="5px"/>
+        <Divider orientation='vertical' size="5px" width="5px" />
         <Flex flexDirection="column" alignItems="center" justifyContent="center" w="50%">
           <p>Software Engineer Graduate and Blockchain Developer</p>
           <p>Working as a freelancer, get in touch with me to collaborate!</p>
         </Flex>
       </Flex>
-    </Center>
+      <Flex height="10%" width="100%">
+        <motion.div
+          initial={{ x: '-2vw' }}
+          animate={{ x: '100vw' }}
+          transition={{ repeat: Infinity, duration: 12 }}
+          style={{ marginLeft: 0 }}
+        >
+          <p>Address</p>
+        </motion.div>
+      </Flex>
+    </Flex >
   );
 };
 
