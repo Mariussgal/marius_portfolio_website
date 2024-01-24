a88cd3f995416714c1724de33106d22233665618
JEAN GAL <jgatbkk@gmail.com>
2024-01-23 19:32:52 -0500

    changed color of media icons and make them a row

--- components/Medias.tsx
+++ components/Medias.tsx
@@ -1,31 +1,33 @@
-import { Box, Flex, IconButton } from '@chakra-ui/react';
+import { Flex, IconButton } from '@chakra-ui/react';
 import { FaLinkedin, FaGithub, FaEnvelope, FaTwitter } from "react-icons/fa";
 
 export default function Medias() {
     return (
-        <Box position="fixed" right="0" top="50%">
-            <Flex direction="column" align="center" justify="center" transform="translateY(-50%)">
-                <IconButton as="a" href="mailto:jeangal.pro@gmail.com" target="_blank"
-                    aria-label="Mail"
-                    icon={<FaEnvelope size="24px" />}
-                    color="black"
-                />
-                <IconButton as="a" href="https://twitter.com/jeangalbc" target="_blank"
-                    aria-label="Twitter"
-                    icon={<FaTwitter size="24px" />}
-                    color="black"
-                />
-                <IconButton as="a" href="https://www.linkedin.com/in/jean-gal/" target="_blank"
-                    aria-label="LinkedIn"
-                    icon={<FaLinkedin size="24px" />}
-                    color="black"
-                />
-                <IconButton as="a" href="https://github.com/jjeangal" target="_blank"
-                    aria-label="Github"
-                    icon={<FaGithub size="24px" />}
-                    color="black"
-                />
-            </Flex>
-        </Box>
+        <Flex direction="row" align="center" justify="center" mr="2%">
+            <IconButton as="a" href="mailto:jeangal.pro@gmail.com" target="_blank" mr="2%"
+                aria-label="Mail"
+                icon={<FaEnvelope size="24px" />}
+                color="white"
+                bg="none"
+            />
+            <IconButton as="a" href="https://twitter.com/jeangalbc" target="_blank" mr="2%"
+                aria-label="Twitter"
+                icon={<FaTwitter size="24px" />}
+                color="white"
+                bg="none"
+            />
+            <IconButton as="a" href="https://www.linkedin.com/in/jean-gal/" target="_blank" mr="2%"
+                aria-label="LinkedIn"
+                icon={<FaLinkedin size="24px" />}
+                color="white"
+                bg="none"
+            />
+            <IconButton as="a" href="https://github.com/jjeangal" target="_blank"
+                aria-label="Github"
+                icon={<FaGithub size="24px" />}
+                color="white"
+                bg="none"
+            />
+        </Flex>
     );
 }
