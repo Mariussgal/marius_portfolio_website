0383f2076c545a634c0d9bdd5374c76406581232
JEAN GAL <jgatbkk@gmail.com>
2024-01-25 00:56:45 -0500

    added a icon link to crypto jobs

--- components/Medias.tsx
+++ components/Medias.tsx
@@ -1,10 +1,23 @@
-import { Flex, IconButton } from '@chakra-ui/react';
+import { Flex, IconButton, Image, Box } from '@chakra-ui/react';
 import { FaLinkedin, FaGithub, FaEnvelope, FaTwitter } from "react-icons/fa";
 
 export default function Medias() {
     return (
-        <Flex direction="row" align="center" justify="center" mr="2%">
-            <IconButton as="a" href="mailto:jeangal.pro@gmail.com" target="_blank" mr="2%"
+        <Flex width="25%" direction="row" align="center" justify="right" mr="2%">
+            <Flex width="24px" justify="center">
+                <Box as="a" href="https://cryptojobslist.com/talent/profile/crypto-professional-remote-9qkhu5ocpn6" target="_blank"
+                    title="Hire me on Crypto Jobs List"
+                    rel="noopener"
+                    width="24px"
+                    height="24px"
+                    _hover={{
+                        backgroundColor: 'rgba(255, 255, 255, 0.1)'
+                    }}
+                >
+                    <Image src="https://cryptojobslist.com/images/logo-circle-white.svg" width="24px" height="24px" objectFit="contain" alt="Hire me on Crypto Jobs List" />
+                </Box>
+            </Flex>
+            <IconButton as="a" href="mailto:jeangal.pro@gmail.com" target="_blank" ml="2%" mr="2%"
                 aria-label="Mail"
                 icon={<FaEnvelope size="24px" />}
                 color="white"
