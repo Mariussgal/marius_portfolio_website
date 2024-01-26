b3b626df2ea661096cbf336f92d07705c4c63326
JEAN GAL <jgatbkk@gmail.com>
2024-01-26 14:24:38 -0500

    styling of header, part 1

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,26 +1,31 @@
-import { Flex, HStack, Spacer, Box, Link } from "@chakra-ui/react";
+import { Flex, HStack, Box, Link, Text } from "@chakra-ui/react";
 import Medias from './Medias';
+import { useState } from 'react';
 
 export default function Header() {
-    const siteTitle = "Home"
+    const [hoveredLink, setHoveredLink] = useState<string | null>(null);
 
     return (
-        <Flex h="10vh" borderBottom="1px">
-            <HStack spacing="4vw" w="30%" ml="2%">
-                <Box>
-                    <Link href="/" fontSize="lg" fontWeight="bold">{siteTitle}</Link>
-                </Box>
-                <Box>
-                    <Link href="/about" fontSize="lg" fontWeight="bold">About</Link>
-                </Box>
-                <Box>
-                    <Link href="/projects" fontSize="lg" fontWeight="bold">Work</Link>
-                </Box>
-                <Box>
-                    <Link href="/poaps" fontSize="lg" fontWeight="bold">POAPs</Link>
-                </Box>
-            </HStack>
-            <Spacer />
+        <Flex h="10vh" borderBottom="1px" justifyContent="space-between">
+            <Box display="flex" alignItems="center" width="25%">
+                <Text fontSize="lg" ml='20%'>{"<JeanGal />"}</Text>
+            </Box>
+            <Flex justifyContent="center">
+                <HStack spacing="4vw" >
+                    <Box onMouseEnter={() => setHoveredLink('home')} onMouseLeave={() => setHoveredLink(null)}>
+                        <Link href="/" fontSize="lg" color={hoveredLink === 'home' ? 'white' : 'lightgray'} _hover={{ textDecoration: 'none' }}>// home</Link>
+                    </Box>
+                    <Box onMouseEnter={() => setHoveredLink('about')} onMouseLeave={() => setHoveredLink(null)}>
+                        <Link href="/about" fontSize="lg" color={hoveredLink === 'about' ? 'white' : 'lightgray'} _hover={{ textDecoration: 'none' }}>//  about</Link>
+                    </Box>
+                    <Box onMouseEnter={() => setHoveredLink('work')} onMouseLeave={() => setHoveredLink(null)}>
+                        <Link href="/projects" fontSize="lg" color={hoveredLink === 'work' ? 'white' : 'lightgray'} _hover={{ textDecoration: 'none' }}>//  work</Link>
+                    </Box>
+                    <Box onMouseEnter={() => setHoveredLink('poaps')} onMouseLeave={() => setHoveredLink(null)}>
+                        <Link href="/poaps" fontSize="lg" color={hoveredLink === 'poaps' ? 'white' : 'lightgray'} _hover={{ textDecoration: 'none' }}>//  poaps</Link>
+                    </Box>
+                </HStack>
+            </Flex>
             <Medias />
         </Flex>
     );
