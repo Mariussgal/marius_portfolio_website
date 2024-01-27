2aa5dcd7dcad23cd00c30e8592f39c7ef5ea826d
JEAN GAL <jgatbkk@gmail.com>
2024-01-27 15:42:05 -0500

    responsiveness of icons in medias header to become menu

--- components/Medias.tsx
+++ components/Medias.tsx
@@ -1,42 +1,89 @@
-import { Flex, IconButton } from '@chakra-ui/react';
-import { FaLinkedin, FaGithub, FaEnvelope } from "react-icons/fa";
+import { Flex, IconButton, useBreakpointValue, Menu, MenuButton, MenuItem, MenuList, Box, Spacer, Text } from '@chakra-ui/react';
+import { FaLinkedin, FaGithub, FaEnvelope, FaBars } from "react-icons/fa";
 import { FaXTwitter } from "react-icons/fa6";
 import CryptoJobs from '../icons/CryptoJobs';
 
 export default function Medias() {
+    const isSmallScreen = useBreakpointValue({ base: true, md: false });
+
     return (
-        <Flex direction="row" align="center" justify="right" mr="2%" width="25%">
-            <IconButton as="a" href="https://cryptojobslist.com/talent/profile/crypto-professional-remote-9qkhu5ocpn6" target="_blank" mr="1%"
-                aria-label="Mail"
-                icon={<CryptoJobs />}
-                color="#DCD7C9"
-                bg="none"
-            />
-            <IconButton as="a" href="mailto:jeangal.pro@gmail.com" target="_blank" mr="1%"
-                aria-label="Mail"
-                icon={<FaEnvelope size="20px" />}
-                color="#DCD7C9"
-                bg="none"
-                title="Hit me up!"
-            />
-            <IconButton as="a" href="https://twitter.com/jeangalbc" target="_blank" mr="1%"
-                aria-label="Twitter"
-                icon={<FaXTwitter size="20px" />}
-                color="#DCD7C9"
-                bg="none"
-            />
-            <IconButton as="a" href="https://www.linkedin.com/in/jean-gal/" target="_blank" mr="1%"
-                aria-label="LinkedIn"
-                icon={<FaLinkedin size="20px" />}
-                color="#DCD7C9"
-                bg="none"
-            />
-            <IconButton as="a" href="https://github.com/jjeangal" target="_blank"
-                aria-label="Github"
-                icon={<FaGithub size="20px" />}
-                color="#DCD7C9"
-                bg="none"
-            />
+        <Flex direction="row" align="center" justify="right" mr="2%" width="25%" color="#DCD7C9">
+            {isSmallScreen ? (
+                <Menu>
+                    <MenuButton as={IconButton} aria-label="Options" icon={<FaBars color='#DCD7C9' />} bg="none" />
+                    <MenuList>
+                        <MenuItem as="a" href="https://cryptojobslist.com/talent/profile/crypto-professional-remote-9qkhu5ocpn6" target="_blank">
+                            <Box display="flex" alignItems="center">
+                                <CryptoJobs />
+                                <Spacer width="5px" />
+                                <Text>Crypto Jobs List</Text>
+                            </Box>
+                        </MenuItem>
+                        <MenuItem as="a" href="mailto:jeangal.pro@gmail.com" target="_blank">
+                            <Box display="flex" alignItems="center">
+                                <FaEnvelope size="20px" />
+                                <Spacer width="5px" />
+                                <Text>Email</Text>
+                            </Box>
+                        </MenuItem>
+                        <MenuItem as="a" href="https://twitter.com/jeangalbc" target="_blank">
+                            <Box display="flex" alignItems="center">
+                                <FaXTwitter size="20px" />
+                                <Spacer width="5px" />
+                                <Text>Twitter / X</Text>
+                            </Box>
+                        </MenuItem>
+                        <MenuItem as="a" href="https://www.linkedin.com/in/jean-gal/" target="_blank">
+                            <Box display="flex" alignItems="center">
+                                <FaLinkedin size="20px" />
+                                <Spacer width="5px" />
+                                <Text>Linkedin</Text>
+                            </Box>
+                        </MenuItem>
+                        <MenuItem as="a" href="https://github.com/jjeangal" target="_blank">
+                            <Box display="flex" alignItems="center">
+                                <FaGithub size="20px" />
+                                <Spacer width="5px" />
+                                <Text>Github</Text>
+                            </Box>
+                        </MenuItem>
+                    </MenuList>
+                </Menu>
+            ) : (
+                <>
+                    <IconButton as="a" href="https://cryptojobslist.com/talent/profile/crypto-professional-remote-9qkhu5ocpn6" target="_blank" mr="1%"
+                        aria-label="Mail"
+                        icon={<CryptoJobs />}
+                        color="#DCD7C9"
+                        bg="none"
+                    />
+                    <IconButton as="a" href="mailto:jeangal.pro@gmail.com" target="_blank" mr="1%"
+                        aria-label="Mail"
+                        icon={<FaEnvelope size="20px" />}
+                        color="#DCD7C9"
+                        bg="none"
+                        title="Hit me up!"
+                    />
+                    <IconButton as="a" href="https://twitter.com/jeangalbc" target="_blank" mr="1%"
+                        aria-label="Twitter"
+                        icon={<FaXTwitter size="20px" />}
+                        color="#DCD7C9"
+                        bg="none"
+                    />
+                    <IconButton as="a" href="https://www.linkedin.com/in/jean-gal/" target="_blank" mr="1%"
+                        aria-label="LinkedIn"
+                        icon={<FaLinkedin size="20px" />}
+                        color="#DCD7C9"
+                        bg="none"
+                    />
+                    <IconButton as="a" href="https://github.com/jjeangal" target="_blank"
+                        aria-label="Github"
+                        icon={<FaGithub size="20px" />}
+                        color="#DCD7C9"
+                        bg="none"
+                    />
+                </>
+            )}
         </Flex>
     );
 }
