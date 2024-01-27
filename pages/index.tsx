7f44e3411411f391e31c97922a97fb919c98390f
JEAN GAL <jgatbkk@gmail.com>
2024-01-27 14:31:08 -0500

    styling of home page, + border around image (still needs some fixes)

--- pages/index.tsx
+++ pages/index.tsx
@@ -16,7 +16,7 @@ const Home: NextPage = () => {
   const address = '0x4df30AF0237E9a5c29D0f49a18Cb6f46692e3c71';
 
   return (
-    <Flex flexDirection="column" height="90vh" width="100%" color="black">
+    <Flex flexDirection="column" height="90vh" width="100%" color="#DCD7C9">
       <Flex
         height="90%"
         width="100%"
@@ -26,13 +26,13 @@ const Home: NextPage = () => {
         <Flex flexDirection="column" alignItems="center" justifyContent="center" w="50%">
           <Text fontSize="x-large">Hi, my name is Jean Gal <br /> and welcome to my website!</Text>
         </Flex>
-        <Image src={"./mepic.png"} alt='Jean Gal' height="45%" width="45%" objectFit="contain" alignSelf="center" />
+        <Image src={"./mepic.png"} alt='Jean Gal' height="45%" width="45%" objectFit="cover" alignSelf="center" sx={{ filter: 'brightness(80%)' }} border="2px" borderColor="#DCD7C9" borderRadius="full" />
         <Flex flexDirection="column" alignItems="center" justifyContent="flex-start" w="50%">
           <Text fontSize="x-large">I am passionate Software Engineer  <br /> with a focus on Blockchain technology</Text>
         </Flex>
       </Flex>
       <Box display="flex" justifyContent="center" height="10%" width="100%" mb={5}>
-        <Box display="flex" flexDirection="row" alignItems="center" height="auto" border="1px solid black" boxSizing="border-box" borderRadius="10px" maxWidth="100%">
+        <Box display="flex" flexDirection="row" alignItems="center" height="auto" border="1px solid #DCD7C9" boxSizing="border-box" borderRadius="10px" maxWidth="100%">
           <Marquee direction="right" speed={50} gradient={false} pauseOnHover={true}>
             <Text m="10px auto" mr={5} transition="all .15s linear" >
               JEAN GAL &copy; {new Date().getFullYear()}
