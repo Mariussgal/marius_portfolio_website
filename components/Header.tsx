c9b386726a2a1ebb7565e52d03a74e4add75e601
JEAN GAL <jgatbkk@gmail.com>
2024-02-17 14:18:42 -0500

    tabs smaller with responsivenes

--- components/Header.tsx
+++ components/Header.tsx
@@ -9,19 +9,19 @@ export default function Header() {
         <Flex h="10vh" borderBottom="1px" justifyContent="space-between">
             <Box isTruncated display="flex" alignItems="center" width="25%" ml="5%">
                 <Box isTruncated onMouseEnter={() => setHoveredLink('home')} onMouseLeave={() => setHoveredLink(null)}>
-                    <Link href="/" fontSize={{ md: "x-large", base: "large" }} color={hoveredLink === 'home' ? 'white' : '#DCD7C9'} _hover={{ textDecoration: 'none' }}>{"<JeanGal />"}</Link>
+                    <Link href="/" fontSize={{ md: "x-large", base: "lg" }} color={hoveredLink === 'home' ? 'white' : '#DCD7C9'} _hover={{ textDecoration: 'none' }}>{"<JeanGal />"}</Link>
                 </Box>
             </Box>
             <Flex justifyContent="center" width="50%">
                 <HStack spacing="4vw" >
                     <Box isTruncated onMouseEnter={() => setHoveredLink('work')} onMouseLeave={() => setHoveredLink(null)}>
-                        <Link href="/projects" fontSize="lg" color={hoveredLink === 'work' ? 'white' : '#DCD7C9'} _hover={{ textDecoration: 'none' }}>{'//  work'}</Link>
+                        <Link href="/projects" fontSize={{ md: "lg", base: "md" }} color={hoveredLink === 'work' ? 'white' : '#DCD7C9'} _hover={{ textDecoration: 'none' }}>{'//  work'}</Link>
                     </Box>
                     <Box isTruncated onMouseEnter={() => setHoveredLink('about')} onMouseLeave={() => setHoveredLink(null)}>
-                        <Link href="/about" fontSize="lg" color={hoveredLink === 'about' ? 'white' : '#DCD7C9'} _hover={{ textDecoration: 'none' }}>{'//  about'}</Link>
+                        <Link href="/about" fontSize={{ md: "lg", base: "md" }} color={hoveredLink === 'about' ? 'white' : '#DCD7C9'} _hover={{ textDecoration: 'none' }}>{'//  about'}</Link>
                     </Box>
                     <Box isTruncated onMouseEnter={() => setHoveredLink('poaps')} onMouseLeave={() => setHoveredLink(null)}>
-                        <Link href="/poaps" fontSize="lg" color={hoveredLink === 'poaps' ? 'white' : '#DCD7C9'} _hover={{ textDecoration: 'none' }}>{'//  poaps'}</Link>
+                        <Link href="/poaps" fontSize={{ md: "lg", base: "md" }} color={hoveredLink === 'poaps' ? 'white' : '#DCD7C9'} _hover={{ textDecoration: 'none' }}>{'//  poaps'}</Link>
                     </Box>
                 </HStack>
             </Flex>
