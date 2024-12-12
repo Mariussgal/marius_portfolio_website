839a907b9c05e6b4f73c3c6e331731b83acf3156
mariussgal <marius.gal@esme.fr>
2024-12-12 17:43:11 +0100

    mobile layout update

--- pages/layout.tsx
+++ pages/layout.tsx
@@ -15,6 +15,7 @@ const Layout: React.FC<Props> = (props) => {
         <Box>
             <Head>
                 <title>Portfolio App</title>
+                <meta name="viewport" content="width=device-width, initial-scale=1" />
                 <link rel="icon" href="/favicon1.png" />
             </Head>
             <Flex direction="column" minHeight="100vh" bgGradient="linear(to-br, red.800, purple.900, black)">
