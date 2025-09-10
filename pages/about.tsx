326f1da0b9a51a5c39c3a66bc4795fb4f380f3de
Marius <151232833+Mariussgal@users.noreply.github.com>
2025-09-10 14:23:39 +0200

    Refactor About component layout and content for improved readability and structure. Update education and experience sections, enhance skills and hobbies presentation, and ensure consistent formatting throughout.

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,17 +1,25 @@
-import type { NextPage } from 'next';
-import { useState, useEffect } from 'react';
+import type { NextPage } from "next";
+import { useState, useEffect } from "react";
 import { Heading } from "@chakra-ui/layout";
-import { Box, Flex,  Circle, Grid } from '@chakra-ui/react';
-import * as Icons from '../icons';
-import { FaGithub, FaExternalLinkAlt } from "react-icons/fa";
-import { Link, Icon } from '@chakra-ui/react';
-
-
+import { Box, Flex, Circle, Grid } from "@chakra-ui/react";
+import * as Icons from "../icons";
+import { FaGithub } from "react-icons/fa";
+import { Link, Icon } from "@chakra-ui/react";
 
 const About: NextPage = () => {
-  const devSkills = [ "TypeScript", "JavaScript", "Nextjs", "React", "Node.js", "Python", "HTML", "Solidity"];
+  const devSkills = [
+    "TypeScript",
+    "JavaScript",
+    "Nextjs",
+    "React",
+    "Node.js",
+    "Python",
+    "HTML",
+    "Solidity",
+  ];
   const [isClient, setIsClient] = useState(false);
-  const cards = [{
+  const cards = [
+    {
       githubLink: "https://github.com/Mariussgal/Defib_simulation",
     },
   ];
@@ -22,65 +30,116 @@ const About: NextPage = () => {
 
   return (
     <Flex as="div" flexDirection="column" width="100%" color="#DCD7C9" p={8}>
-      
-  
-      
-      <Flex direction={{ base: 'column', md: 'row' }} gap={8} alignItems="stretch">
-        
-        <Flex flexDirection="column" flex="1" p={4} bg="gray.800" borderRadius="md" boxShadow="md">
+      <Flex
+        direction={{ base: "column", md: "row" }}
+        gap={8}
+        alignItems="stretch"
+      >
+        <Flex
+          flexDirection="column"
+          flex="1"
+          p={4}
+          bg="gray.800"
+          borderRadius="md"
+          boxShadow="md"
+        >
           <Box mb={4}>
             <Heading size="lg">EDUCATION</Heading>
           </Box>
           <Box h="2px" bg="#DCD7C9" mb={4} />
-  
-          
-          <Flex flexDirection="column" gap={4}>
-            
-            <Flex flexDirection="column">
-              <Box><b>ESME</b> Paris</Box>
-              <Box textAlign="center"> Second-Year General Engineering (International, English-Taught Program) </Box>
+
+          <Flex
+            flexDirection="column"
+            gap={4}
+            flex="1"
+            justifyContent="space-between"
+          >
+            <Flex
+              flexDirection="column"
+              flex="1"
+              justifyContent="center"
+              py={2}
+            >
+              <Box>
+                <b>ESME</b> Paris
+              </Box>
+              <Box textAlign="center">
+                {" "}
+                Third-Year General Engineering (International, English-Taught
+                Program){" "}
+              </Box>
               <Box textAlign="right">Sept. 2023 - July 2028</Box>
             </Flex>
             <Box h="1px" bg="#DCD7C9" />
-  
-           
-            <Flex flexDirection="column">
-              <Box>Lycée Montalembert, <b>Courbevoie</b></Box>
-              <Box textAlign="center"> Science Baccalaureate with Specializations in Mathematics and Physics </Box>
+
+            <Flex
+              flexDirection="column"
+              flex="1"
+              justifyContent="center"
+              py={2}
+            >
+              <Box>
+                Lycée Montalembert, <b>Courbevoie</b>
+              </Box>
+              <Box textAlign="center">
+                {" "}
+                Science Baccalaureate with Specializations in Mathematics and
+                Physics{" "}
+              </Box>
               <Box textAlign="right">Sept. 2020 - July 2023</Box>
             </Flex>
             <Box h="1px" bg="#DCD7C9" />
-  
-            
-            <Flex flexDirection="column">
-              <Box>Collège Sainte Geneviève, <b>Courbevoie</b></Box>
+
+            <Flex
+              flexDirection="column"
+              flex="1"
+              justifyContent="center"
+              py={2}
+            >
+              <Box>
+                Collège Sainte Geneviève, <b>Courbevoie</b>
+              </Box>
               <Box textAlign="center">Middle school</Box>
               <Box textAlign="right">2018 - 2020</Box>
             </Flex>
             <Box h="1px" bg="#DCD7C9" />
-  
-            
-            <Flex flexDirection="column">
-              <Box>Lycée Lyautey, <b>Casablanca</b></Box>
+
+            <Flex
+              flexDirection="column"
+              flex="1"
+              justifyContent="center"
+              py={2}
+            >
+              <Box>
+                Lycée Lyautey, <b>Casablanca</b>
+              </Box>
               <Box textAlign="center">Middle school</Box>
               <Box textAlign="right">2016 - 2018</Box>
             </Flex>
           </Flex>
         </Flex>
 
-       
-        <Flex flexDirection="column" flex="1" p={4} bg="gray.800" borderRadius="md" boxShadow="md">
+        <Flex
+          flexDirection="column"
+          flex="1"
+          p={4}
+          bg="gray.800"
+          borderRadius="md"
+          boxShadow="md"
+        >
           <Box mb={4}>
             <Heading size="lg">EXPERIENCE</Heading>
           </Box>
           <Box h="2px" bg="#DCD7C9" mb={4} />
-  
-          
+
           <Flex flexDirection="column" gap={4}>
-          <Box>
-              <b>Saint-Louis Hospital (AP-HP) - Web Development internship</b>,<br /> <br /> During my 2 months long internship, I had the opportunity to work on a project combining healthcare, education, and technology: the development of an online defibrillator simulation platform..
+            <Box>
+              <b>Saint-Louis Hospital (AP-HP) - Web Development internship</b>,
+              <br /> <br /> During my 2 months long internship, I had the
+              opportunity to work on a project combining healthcare, education,
+              and technology: the development of an online defibrillator
+              simulation platform..
               <Flex justifyContent="space-between" alignItems="center" mt={1}>
-                
                 {cards[0].githubLink && (
                   <Link href={cards[0].githubLink} isExternal>
                     <Icon as={FaGithub} />
@@ -91,183 +150,220 @@ const About: NextPage = () => {
             </Box>
             <Box h="1px" bg="#DCD7C9" />
             <Box>
-              <b>D-MAX (Moving company)</b>,<br /> <br />Successfully completed more than 50 moves, ensuring efficient organization and execution. <br />Applied effective lifting techniques to safely handle heavy loads and prevent injuries.
+              <b>D-MAX (Moving company)</b>,<br /> <br />
+              Successfully completed more than 50 moves, ensuring efficient
+              organization and execution. <br />
+              Applied effective lifting techniques to safely handle heavy loads
+              and prevent injuries.
               <Box textAlign="right">June - July 2022/23/24</Box>
             </Box>
             <Box h="1px" bg="#DCD7C9" />
             <Box>
-              <b>Levallois entraide (volunteering)</b>, <br /><br />Collected clothing, food, and essential supplies for humanitarian aid in Ukraine. <br />Participated in initiatives to support underprivileged individuals.  
+              <b>Levallois entraide (volunteering)</b>, <br />
+              <br />
+              Collected clothing, food, and essential supplies for humanitarian
+              aid in Ukraine. <br />
+              Participated in initiatives to support underprivileged
+              individuals.
               <Box textAlign="right">Oct. 2021 - June 2022</Box>
             </Box>
             <Box h="1px" bg="#DCD7C9" />
             <Box>
-              <b>Les terrasses du clapas (Waiter and dishwasher)</b>,<br /><br /> Took customer orders and provided attentive table service in a fast-paced environment. <br />Managed high-pressure situations, ensuring smooth service and customer satisfaction.
+              <b>Les terrasses du clapas (Waiter and dishwasher)</b>,<br />
+              <br /> Took customer orders and provided attentive table service
+              in a fast-paced environment. <br />
+              Managed high-pressure situations, ensuring smooth service and
+              customer satisfaction.
               <Box textAlign="right">july 2021</Box>
             </Box>
-            
           </Flex>
         </Flex>
-  
-       
       </Flex>
-  
+
       <Flex flexDirection="column" gap={8} p={8}>
-
-<Flex flexDirection={{ base: "column", md: "row" }} gap={8} alignItems="stretch">
-      
-
-<Flex flexDirection="column" flex="1" p={4} bg="gray.800" borderRadius="md" boxShadow="md">
-<Box mb={4}>
-  <Heading size="lg">LANGUAGES</Heading>
-</Box>
-<Box h="2px" bg="#DCD7C9" mb={4} />
-<Flex flexDirection="column" gap={4} flex="1">
-  
-  <Flex flex="1" justify="space-between" alignItems="center">
-    <Box flex="1">
-      <b>French</b>
- 
-    <Box textAlign="right" flex="1"> Native </Box>
-    </Box>
-  </Flex>
-  <Box h="1px" bg="#DCD7C9" />
-  
-  
-  <Flex flex="1" justify="space-between" alignItems="center">
-    <Box flex="1">
-      <b>English</b>
-    
-    <Box textAlign="right" flex="1">
-      Fluent
-    </Box>
-    </Box>
-  </Flex>
-  <Box h="1px" bg="#DCD7C9" />
-
-  
-  <Flex flex="1" justify="space-between" alignItems="center">
-    <Box flex="1">
-      <b>Spanish</b>
-    
-    <Box textAlign="right" flex="1">
-      Beginner
-    </Box>
-    </Box>
-  </Flex>
-
-</Flex>
-
-  </Flex>
-  <Flex flexDirection="column" flex="1" p={4} bg="gray.800" borderRadius="md" boxShadow="md">
-    <Box mb={4}>
-      <Heading size="lg">SKILLS</Heading>
-    </Box>
-    <Box h="2px" bg="#DCD7C9" mb={4} />
-    <Flex flexDirection="column" gap={4}>
-    <Box h="auto" w="100%"  color="#DCD7C9">
-          <Flex>
-            <Box display={{ base: "none", md: "flex" }} >
-              <Icons.Dev />
+        <Flex
+          flexDirection={{ base: "column", md: "row" }}
+          gap={8}
+          alignItems="stretch"
+        >
+          <Flex
+            flexDirection="column"
+            flex="1"
+            p={4}
+            bg="gray.800"
+            borderRadius="md"
+            boxShadow="md"
+          >
+            <Box mb={4}>
+              <Heading size="lg">LANGUAGES</Heading>
             </Box>
-            <Heading ml={2} size="md">DEVELOPMENT, (currently learning)</Heading>
+            <Box h="2px" bg="#DCD7C9" mb={4} />
+            <Flex flexDirection="column" gap={4} flex="1">
+              <Flex flex="1" justify="space-between" alignItems="center">
+                <Box flex="1">
+                  <b>French</b>
+
+                  <Box textAlign="right" flex="1">
+                    {" "}
+                    Native{" "}
+                  </Box>
+                </Box>
+              </Flex>
+              <Box h="1px" bg="#DCD7C9" />
+
+              <Flex flex="1" justify="space-between" alignItems="center">
+                <Box flex="1">
+                  <b>English</b>
+
+                  <Box textAlign="right" flex="1">
+                    Fluent
+                  </Box>
+                </Box>
+              </Flex>
+              <Box h="1px" bg="#DCD7C9" />
+
+              <Flex flex="1" justify="space-between" alignItems="center">
+                <Box flex="1">
+                  <b>Spanish</b>
+
+                  <Box textAlign="right" flex="1">
+                    Beginner
+                  </Box>
+                </Box>
+              </Flex>
+            </Flex>
           </Flex>
-    <Grid templateColumns={["repeat(2, 1fr)", "repeat(2, 1fr)", "repeat(3, 1fr)", "repeat(4, 1fr)"]} fontSize="small" mt={4} gap={3} color="#DCD7C9">
-            {devSkills.map((skill, index) => (
-              <Circle key={index} size="75px" borderColor="#DCD7C9" borderWidth={1}>
-                {skill}
-              </Circle>
-            ))}
-          </Grid>
-          </Box>
-      
-          <Flex alignItems="center">
+          <Flex
+            flexDirection="column"
+            flex="1"
+            p={4}
+            bg="gray.800"
+            borderRadius="md"
+            boxShadow="md"
+          >
+            <Box mb={4}>
+              <Heading size="lg">SKILLS</Heading>
+            </Box>
+            <Box h="2px" bg="#DCD7C9" mb={4} />
+            <Flex flexDirection="column" gap={4}>
+              <Box h="auto" w="100%" color="#DCD7C9">
+                <Flex>
+                  <Box display={{ base: "none", md: "flex" }}>
+                    <Icons.Dev />
+                  </Box>
+                  <Heading ml={2} size="md">
+                    DEVELOPMENT, (currently learning)
+                  </Heading>
+                </Flex>
+                <Grid
+                  templateColumns={[
+                    "repeat(2, 1fr)",
+                    "repeat(2, 1fr)",
+                    "repeat(3, 1fr)",
+                    "repeat(4, 1fr)",
+                  ]}
+                  fontSize="small"
+                  mt={4}
+                  gap={3}
+                  color="#DCD7C9"
+                >
+                  {devSkills.map((skill, index) => (
+                    <Circle
+                      key={index}
+                      size="75px"
+                      borderColor="#DCD7C9"
+                      borderWidth={1}
+                    >
+                      {skill}
+                    </Circle>
+                  ))}
+                </Grid>
+              </Box>
+
+              <Flex alignItems="center">
                 <Box display={{ base: "none", md: "flex" }}>
                   <Icons.Tools />
                 </Box>
-                <Heading ml={2} size="md">Tools</Heading>
+                <Heading ml={2} size="md">
+                  Tools
+                </Heading>
               </Flex>
-      <Box>
-        
-       Git, VSCode, Remix IDE
-      </Box>
-    </Flex>
-  </Flex>
-
-
-
-  
-  <Flex alignItems = "stretch" flexDirection="column" flex="1" p={4} bg="gray.800" borderRadius="md" boxShadow="md">
-    <Box mb={4}>
-      <Heading size="lg">ASSETS</Heading>
-    </Box>
-    <Box h="2px" bg="#DCD7C9" mb={4} />
-    
-    <Flex flex="1" justify="space-between" alignItems="center">
-          <Box>
-            <b>Team spirit</b> 
- 
-          </Box>
+              <Box>Git, VSCode, Remix IDE</Box>
+            </Flex>
           </Flex>
-          <Box h="1px" bg="#DCD7C9" />
-          <Flex flex="1" justify="space-between" alignItems="center">
-          <Box>
-            <b>Adaptability and flexibility</b> 
-   
-          </Box>
-          </Flex>
-          <Box h="1px" bg="#DCD7C9" />
-          <Flex flex="1" justify="space-between" alignItems="center">
-          <Box>
-            <b>Sens of organization</b>
 
-          </Box>
+          <Flex
+            alignItems="stretch"
+            flexDirection="column"
+            flex="1"
+            p={4}
+            bg="gray.800"
+            borderRadius="md"
+            boxShadow="md"
+          >
+            <Box mb={4}>
+              <Heading size="lg">ASSETS</Heading>
+            </Box>
+            <Box h="2px" bg="#DCD7C9" mb={4} />
+
+            <Flex flex="1" justify="space-between" alignItems="center">
+              <Box>
+                <b>Team spirit</b>
+              </Box>
+            </Flex>
+            <Box h="1px" bg="#DCD7C9" />
+            <Flex flex="1" justify="space-between" alignItems="center">
+              <Box>
+                <b>Adaptability and flexibility</b>
+              </Box>
+            </Flex>
+            <Box h="1px" bg="#DCD7C9" />
+            <Flex flex="1" justify="space-between" alignItems="center">
+              <Box>
+                <b>Sens of organization</b>
+              </Box>
+            </Flex>
           </Flex>
-          </Flex>
-        
- 
-    </Flex>
-  </Flex>
+        </Flex>
+      </Flex>
 
       <Box color="#DCD7C9" mt={8}>
-       
-        <Flex flexDirection="column" flex="1" p={4} bg="gray.800" borderRadius="md" boxShadow="md">
+        <Flex
+          flexDirection="column"
+          flex="1"
+          p={4}
+          bg="gray.800"
+          borderRadius="md"
+          boxShadow="md"
+        >
           <Box mb={4}>
             <Heading size="lg">HOBBIES</Heading>
           </Box>
           <Box h="2px" bg="#DCD7C9" mb={4} />
-  
+
           <Flex flexDirection="column" gap={4}>
             <Box>
-              <b>Rugby</b>, Espoirs Fédérale 1 level at the Courbevoie rugby club
+              <b>Rugby</b>, Espoirs Fédérale 1 level at the Courbevoie rugby
+              club
               <Box textAlign="right">2012- Present</Box>
             </Box>
             <Box h="1px" bg="#DCD7C9" />
             <Box>
-              <b>Weight lifting</b>, Practiced for 2 years as a supplement to improve my rugby skills
+              <b>Weight lifting</b>, Practiced for 2 years as a supplement to
+              improve my rugby skills
               <Box textAlign="right">2023 - Present</Box>
             </Box>
             <Box h="1px" bg="#DCD7C9" />
             <Box>
-              <b>Running</b>, Completed the half Marathon of Madrid in 1h53  
+              <b>Running</b>, Completed the half Marathon of Madrid in 1h53
               <Box textAlign="right">2021</Box>
             </Box>
             <Box h="1px" bg="#DCD7C9" />
-
           </Flex>
-         </Flex>
+        </Flex>
       </Box>
-
-     
-  </Flex>
-
-
+    </Flex>
   );
-  
-}
+};
 
-
-
-
-export default About
-
-
+export default About;
